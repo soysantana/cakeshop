9348d8b66b54bdb644ca0b88d3305354d910ab05
Santana <arturo.santtana@gmail.com>
2024-06-07 08:10:31 -0400

    📘 docs: first commit

--- src/config/load.php
+++
@@ -1,9 +0,0 @@
-<?php
-function url($rutaRelativa) {
-    $urlBase = dirname($_SERVER['SCRIPT_NAME']);
-    $urlBase = preg_replace('~(?:/[^/]+){2}/?$~', '', $urlBase);
-    $rutaFinal = $urlBase . '/' . ltrim($rutaRelativa, '/');
-    $rutaFinal = preg_replace('/\/+/', '/', $rutaFinal);
-    return $rutaFinal;
-}
-?>
